102a506f711a4b25a9a5b4048431adc71832fd3d
Jhovany.Olvera <jhovany.olvera@qacg.com>
2017-05-24 15:25:04 -0500

    COMMIT INICIAL
    * Se crea el front de SplashScreen de cemedepsi

---
+++ Cemedepsimovil/app/src/main/java/org/cemedepsi/cemedepsimovil/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package org.cemedepsi.cemedepsimovil;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+
+
+}

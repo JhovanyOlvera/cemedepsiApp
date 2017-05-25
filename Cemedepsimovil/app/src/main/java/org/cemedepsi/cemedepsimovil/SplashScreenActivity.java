df92423416ca4328fa1e7d7dddd6bf25ff86af57
Jhovany.Olvera <jhovany.olvera@qacg.com>
2017-05-25 10:22:41 -0500

    * Se agraga animacion en logo del SplashScreen * Se integra Butter Knife

--- Cemedepsimovil/app/src/main/java/org/cemedepsi/cemedepsimovil/SplashScreenActivity.java
+++ Cemedepsimovil/app/src/main/java/org/cemedepsi/cemedepsimovil/SplashScreenActivity.java
@@ -2,15 +2,34 @@ package org.cemedepsi.cemedepsimovil;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SplashScreenActivity extends Activity {
+
+    @BindView(R.id.imgVwLogoAnimate)
+    ImageView imgVwLogoAnimate;
+    @BindView(R.id.txtNameAPP)
+    TextView txtNameAPP;
+    @BindView(R.id.txtVersion)
+    TextView txtVersion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
+        ButterKnife.bind(this);
 
+        Animation rotateImage;
+        rotateImage = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        rotateImage.reset();
+        imgVwLogoAnimate.startAnimation(rotateImage);
+    }
 
 }

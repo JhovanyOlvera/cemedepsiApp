0a12fd3e059bcff6dcf0fe0190caa46f7bd5ea2d
Jhovany.Olvera <jhovany.olvera@qacg.com>
2017-05-25 10:55:16 -0500

    * Se agraga animacion en logo del SplashScreen * Se integra Butter Knife * Se crean Constantes * Se refactoriza el codigo

--- Cemedepsimovil/app/src/main/java/org/cemedepsi/cemedepsimovil/SplashScreenActivity.java
+++ Cemedepsimovil/app/src/main/java/org/cemedepsi/cemedepsimovil/SplashScreenActivity.java
@@ -7,6 +7,8 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.cemedepsi.cemedepsimovil.utils.Constants;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -19,14 +21,24 @@ public class SplashScreenActivity extends Activity {
     @BindView(R.id.txtVersion)
     TextView txtVersion;
 
+    private Animation rotateImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         ButterKnife.bind(this);
+        setLabel();
+        startAnimationLogo();
+    }
 
-        Animation rotateImage;
+    private void setLabel() {
+        txtNameAPP.setText(Constants.APP.NAME_APP);
+        txtVersion.setText(String.format(getString(R.string.app_version), Constants.APP.VERSION_APP));
+    }
+
+    private void startAnimationLogo() {
         rotateImage = AnimationUtils.loadAnimation(this, R.anim.rotate);
         rotateImage.reset();
         imgVwLogoAnimate.startAnimation(rotateImage);
---
+++ Cemedepsimovil/app/src/main/java/org/cemedepsi/cemedepsimovil/utils/Constants.java
@@ -0,0 +1,14 @@
+package org.cemedepsi.cemedepsimovil.utils;
+
+/**
+ * Created by jhovany on 25/05/17.
+ */
+
+public class Constants {
+
+    public static class APP {
+
+        public static final String NAME_APP = "CEMEDEPSI";
+        public static final String VERSION_APP = "1.0";
+    }
+}
